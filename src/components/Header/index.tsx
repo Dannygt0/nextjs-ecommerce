ee4c4d57a01dac01b6664d832d95422a98d0748f
Dannygt0 <dmunozt2@miumg.edu.gt>
2026-03-18 20:47:54 -0600

    feat: ocultar enlace de 'Recently Viewed'

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -334,7 +334,7 @@ const Header = () => {
             {/* // <!--=== Nav Right Start ===--> */}
             <div className="hidden xl:block">
               <ul className="flex items-center gap-5.5">
-                <li className="py-4">
+                {/* <li className="py-4">
                   <a
                     href="#"
                     className="flex items-center gap-1.5 font-medium text-custom-sm text-dark hover:text-blue"
@@ -358,7 +358,7 @@ const Header = () => {
                     </svg>
                     Recently Viewed
                   </a>
-                </li>
+                </li> */}
 
                 <li className="py-4">
                   <Link
