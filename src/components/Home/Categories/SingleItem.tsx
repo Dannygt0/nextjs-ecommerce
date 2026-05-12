0330c34a0c35ea271ca9acbf9178a387bd4f6582
Dannygt0 <dmunozt2@miumg.edu.gt>
2026-05-12 13:23:28 -0600

    feat: implement Contentful CMS for categories section

--- src/components/Home/Categories/SingleItem.tsx
+++ src/components/Home/Categories/SingleItem.tsx
@@ -1,19 +1,25 @@
 import { Category } from "@/types/category";
-import React from "react";
 import Title from '@/components/SubComponent/Title';
 import Image from "next/image";
 
 const SingleItem = ({ item }: { item: Category }) => {
+  const imageUrl = item?.fields?.image?.fields?.file?.url;
+  const itemTitle = item?.fields?.title || "Category"
   return (
     <a href="#" className="group flex flex-col items-center">
       <div className="max-w-[130px] w-full bg-[#F2F3F8] h-32.5 rounded-full flex items-center justify-center mb-4">
-        <Image src={item.img} alt="Category" width={82} height={62} />
+        {imageUrl ? (
+        <Image src={`https:${item.fields.image.fields.file.url}`}alt="Category" width={82} height={62} />
+
+        ):(
+          <div className="text-gray-400 text-xs text-center px-2">No Image</div>
+        )}
       </div>
 
       <div className="flex justify-center">
-        <h3 className="inline-block font-medium text-center text-dark bg-gradient-to-r from-blue to-blue bg-[length:0px_1px] bg-left-bottom bg-no-repeat transition-[background-size] duration-500 hover:bg-[length:100%_3px] group-hover:bg-[length:100%_1px] group-hover:text-blue">
-          {item.title}
-        </h3>
+        <Title className="inline-block font-medium text-center text-dark bg-gradient-to-r from-blue to-blue bg-[length:0px_1px] bg-left-bottom bg-no-repeat transition-[background-size] duration-500 hover:bg-[length:100%_3px] group-hover:bg-[length:100%_1px] group-hover:text-blue">
+          {item.fields.title}
+        </Title>
       </div>
     </a>
   );
